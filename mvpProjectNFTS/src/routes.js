4e67f0552c34235d331b3d1a082a5fb99c1900fc
abdullahyaqoob <65088172+abdullahyaqoob@users.noreply.github.com>
2021-08-17 15:28:31 +0500

    Add files via upload

---
+++ mvpProjectNFTS/src/routes.js
@@ -0,0 +1,8 @@
+import nft from './components/nft.vue';
+// import mainPage from './components/mainPage.vue';
+
+export default [
+    { path: '/', component: nft },
+    // { path: '/', component: mainPage },
+    // { path: '/nft', component: nft },
+]
